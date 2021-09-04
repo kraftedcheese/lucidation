93414dcb28f623e1a8645d96805cdfb12184f473
kraftedcheese <38340979+kraftedcheese@users.noreply.github.com>
2021-09-04 14:04:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# lucidation
+
+Flutter App with Tensorflow Lite integration, producing DeepDream filters on captured images.
---
+++ android/app/src/main/kotlin/com/example/lucidation/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.lucidation
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
